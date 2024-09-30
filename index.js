5738993b53ab300bc8ed2b801a4ffb977015a163
Dnica98 <daniela.nica98@gmail.com>
2024-09-30 20:25:08 +0300

    1

---
+++ index.js
@@ -0,0 +1,134 @@
+// // var let const
+
+// var x;
+
+// let y = 2;
+
+// const z = 3;
+
+
+
+// // y = 'new';
+
+// console.log(y);
+
+// console.log(x);
+
+
+// const num = 1;
+// const str = 'string';
+// const bool = true;
+// const undief = underfined;
+// const nul = null;
+
+
+// const symb = Symbol;
+// const bigInt = BigInt(11);
+// const obj = {};
+
+// const obj1 = {
+//     a:4,
+//     b:5,
+//     c:6
+// };
+
+// const obj2 = {
+//     a:4,
+//     b:5,
+//     c:6
+// }
+
+// obj2.a = 99;
+
+// console.log('obj1', obj1)
+// console.log('obj2', obj2)
+
+// let num1 = 1;
+
+// let num2 = num1;
+
+// num1 = 4;
+// console.log ('num1',num1 )
+// console.log ('num2', num2)
+
+
+// const num1 =3;
+// const num2 = 4;
+
+// console.log(num1+num2)
+
+// const str1 ='string1';
+// const str2 ='string2';
+
+// console.log(num1 + str2)
+
+
+
+
+
+// const bool = false;
+// console.log('type of bool is:', typeof bool)
+
+// const undief = NaN;
+// console.log('type of undief is:', typeof undief)
+
+// const nul = null;
+// console.log('type of nul is:', typeof nul)
+
+// const symb = Symbol;
+// console.log('type of symb is:', typeof symb)
+
+// const bigInt = BigInt(10);
+// console.log('type of bigInt is:', typeof bigInt )
+
+// const obj = { };
+// console.log('type of obj is:', typeof obj)
+
+// const num1 = 5;
+// const num2 = 9;
+// const num3 = 2;
+// const str1 = 'mere';
+// const str2 ='si';
+// const str3 = 'pere';
+
+// console.log(num1+num2+num3)
+// console.log(num1-num2-num3)
+// console.log(str1+' '+str2+' '+str3)
+// console.log(num1+num3+' '+str3)
+// console.log(num1+' '+str1+' '+str2+' '+num2+' '+str3)
+
+console.log(0 == '')
+console.log(0 === '')
+
+
+const falsy1 = 0;
+const falsy2 = '';
+const falsy3 = undefined;
+const falsy4 = null;
+const falsy5 = NaN;
+
+console.log(!falsy1)
+console.log(!falsy2)
+console.log(!falsy3)
+console.log(!falsy4)
+console.log(!falsy5)
+
+console.log(!!falsy1)
+console.log(!!falsy2)
+console.log(!!falsy3)
+console.log(!!falsy4)
+console.log(!!falsy5)
+
+
+let num1 = 15;
+
+console.log(++num1)
+console.log(--num1)
+
+let dcr = 8;
+
+console.log(--dcr)
+
+num1=num1-dcr;
+
+console.log(num1-dcr)
